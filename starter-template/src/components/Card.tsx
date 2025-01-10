9f2bc100ecf49dc5f866626470d5a2fc3a6de437
Navansh Krishna Goswami <navanshgoswami5@gmail.com>
2025-01-10 12:41:35 +0530

    Initial commit

---
+++ starter-template/src/components/Card.tsx
@@ -0,0 +1,25 @@
+import grainImage from "@/assets/images/grain.jpg";
+import { twMerge } from "tailwind-merge";
+import { PropsWithChildren } from "react";
+
+export const Card = ({
+  className,
+  children,
+}: PropsWithChildren<{ className?: string }>) => {
+  return (
+    <div
+      className={twMerge(
+        "bg-gray-800 z-0 overflow-hidden  rounded-3xl relative after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
+        className
+      )}
+    >
+      <div
+        className="absolute insert-0 -z-10 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
+      {children}
+    </div>
+  );
+};
